801a7ebe0604ba22a81489cd1f2619692c3ae7d8
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-03-09 19:12:18 -0800

    Added 2D Array task package

---
+++ wk1_assignments/src/com/ss/tasks/array/IntArray2D.java
@@ -0,0 +1,82 @@
+/**
+ * Week 1 - Day 2 - Array task
+ */
+package com.ss.tasks.array;
+
+/**
+ * Class for 2d Integer Array
+ * @author Lexi Nelson
+ *
+ */
+public class IntArray2D {
+	
+	private Integer[][] arr;
+
+	/**
+	 * Constructor
+	 * @param row
+	 * @param col
+	 */
+	public IntArray2D(Integer row, Integer col) {
+		arr = new Integer[row][col];
+	}
+	
+	/**
+	 * Add value to a slot in array
+	 * @param val
+	 * @param row
+	 * @param col
+	 */
+	public void add(Integer val, Integer row, Integer col) {
+		arr[row][col] = val;
+	}
+	
+	/**
+	 * fills array with random positive numbers under 100
+	 */
+	public void fillRandom() {
+		for (int r = 0; r < arr.length; r++) {
+			for (int c = 0; c < arr[r].length; c++) {
+				add((int) (1 + Math.random() * 99), r, c);
+			}
+		}
+	}
+	
+	/**
+	 * Display table representation of array to console
+	 */
+	public void display() {
+		for (Integer[] array : arr) {
+			for(Integer val : array) {
+				System.out.printf("%4d", val);
+			}
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * @return maximum value in 2D array
+	 */
+	public Integer max() {
+		Integer max = arr[0][0];
+		for (Integer r = 0; r < arr.length; r++) {
+			for (Integer c = 0; c < arr[r].length; c++) {
+				max = (arr[r][c] > max) ? arr[r][c] : max;
+			}
+		}
+		return max;
+	}
+	
+	/**
+	 * @return minimum value in 2D array
+	 */
+	public Integer min() {
+		Integer min = arr[0][0];
+		for (int r = 0; r < arr.length; r++) {
+			for (int c = 0; c < arr[r].length; c++) {
+				min = (arr[r][c] < min) ? arr[r][c] : min;
+			}
+		}
+		return min;
+	}
+}
---
+++ wk1_assignments/src/com/ss/tasks/array/IntArrayTest.java
@@ -0,0 +1,29 @@
+/**
+ * Week 1 - Day 2 - Array task
+ */
+package com.ss.tasks.array;
+
+/**
+ * Test to run IntArray2D class
+ * @author Lexi Nelson
+ *
+ */
+
+public class IntArrayTest {
+
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		IntArray2D myArray = new IntArray2D(5, 7);
+		myArray.fillRandom();
+
+		myArray.display();
+		
+		System.out.println();
+		System.out.println("Max: " + myArray.max());
+		System.out.println("Min: " + myArray.min());
+	}
+
+}
