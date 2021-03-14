1f5fff13ef8be6709c79fc20493eb5fbf184e524
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-03-13 20:09:21 -0800

    Test Skeleton - DateTimeBasic and MyUtils

---
+++ tests/com/ss/firstwk/fri/DateTimeBasicsTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.ss.firstwk.fri;
+
+import org.junit.Test;
+
+/**
+ * @author lexne
+ *
+ */
+public class DateTimeBasicsTest {
+	private final BasicDateTime base = new BasicDateTime();
+	
+	@Test
+	public void getPreviousThursdayTest() {
+		
+	}
+	
+	@Test
+	public void getPreviousThursdayEdgeTest() {
+		
+	}
+	
+	@Test
+	public void zoneIdConversionTest() {
+		
+	}
+	
+	@Test
+	public void zoneIdConversionEdgeTest() {
+		
+	}
+	
+	@Test
+	public void convertTest() {
+		
+	}
+	
+	@Test
+	public void convertEdgeTest() {
+		
+	}
+	
+	@Test
+	public void getMonthLengthsTest() {
+		
+	}
+	
+	@Test
+	public void getMonthLengthsEdgeTest() {
+		
+	}
+	
+	@Test
+	public void listMondaysTest() {
+		
+	}
+	
+	@Test
+	public void listMondaysEdgeTest() {
+		
+	}
+	
+	@Test
+	public void isFirdayThirteenTest() {
+		
+	}
+	
+	@Test
+	public void isFridayThirteenEdgeTest() {
+		
+	}
+}
---
+++ tests/com/ss/firstwk/fri/MyUtilsTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.ss.firstwk.fri;
+
+import org.junit.Test;
+
+/**
+ * @author lexne
+ *
+ */
+public class MyUtilsTest {
+	private final MyUtils base = new MyUtils();
+	
+	@Test
+	public void orderByContentsTest() {
+		
+	}
+	
+	@Test
+	public void orderByContentsEdgeTest() {
+		
+	}
+	
+	@Test
+	public void concatIntArrayTest() {
+		
+	}
+	
+	@Test
+	public void concatIntArrayEdgeTest() {
+		
+	}
+	
+	@Test
+	public void filterByThreeAndATest() {
+		
+	}
+	
+	@Test
+	public void filterByThreeAndAEdgeTest() {
+		
+	}
+}
