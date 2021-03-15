bc86b454d2608e33d43167c566de823c3a23c30a
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-03-15 10:36:27 -0700

    very basic DateTime unit test

--- tests/com/ss/firstwk/fri/DateTimeBasicsTest.java
+++ tests/com/ss/firstwk/fri/DateTimeBasicsTest.java
@@ -3,6 +3,16 @@
  */
 package com.ss.firstwk.fri;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -14,61 +24,38 @@ public class DateTimeBasicsTest {
 	
 	@Test
 	public void getPreviousThursdayTest() {
-		
-	}
-	
-	@Test
-	public void getPreviousThursdayEdgeTest() {
-		
+		LocalDate test = LocalDate.of(2021, 3, 15);
+		LocalDate expected = LocalDate.of(2021, 3, 11);
+		assertEquals(expected, base.getPreviousThursday(test));
 	}
 	
 	@Test
 	public void zoneIdConversionTest() {
-		
+		ZoneId test = ZoneId.of("US/Central");
+		ZoneOffset expected = ZoneOffset.of("-05:00");
+		assertEquals(expected, base.convertZoneIDtoOffset(test));
 	}
 	
 	@Test
-	public void zoneIdConversionEdgeTest() {
-		
-	}
-	
-	@Test
-	public void convertTest() {
-		
-	}
-	
-	@Test
-	public void convertEdgeTest() {
-		
-	}
-	
-	@Test
-	public void getMonthLengthsTest() {
-		
-	}
-	
-	@Test
-	public void getMonthLengthsEdgeTest() {
-		
+	public void getMonthLengthsTest() { 
+		String[] expected = new String[] {"January: 31 days", "February: 28 days", "March: 31 days",
+				"April: 30 days", "May: 31 days", "June: 30 days", "July: 31 days", "August: 31 days",
+				"September: 30 days", "October: 31 days", "November: 30 days", "December: 31 days"};
+		assertArrayEquals(expected, base.getMonthLengths(2021));
 	}
 	
 	@Test
 	public void listMondaysTest() {
-		
-	}
-	
-	@Test
-	public void listMondaysEdgeTest() {
-		
+		List<Integer> expected = Arrays.asList(1, 8, 15, 22, 29);
+		assertEquals(expected, base.listMondays(3));
 	}
 	
 	@Test
 	public void isFirdayThirteenTest() {
+		LocalDate no = LocalDate.of(2021, 3, 15);
+		LocalDate yes = LocalDate.of(2021, 8, 13);
 		
-	}
-	
-	@Test
-	public void isFridayThirteenEdgeTest() {
-		
+		assertNotEquals(true, base.isFridayThirteen(no));
+		assertEquals(true, base.isFridayThirteen(yes));
 	}
 }
